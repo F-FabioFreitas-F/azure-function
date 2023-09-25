48d5025f067495cdd30134690cd1bf43568e2b59
F-FabioFreitas-F <120140384+F-FabioFreitas-F@users.noreply.github.com>
2023-09-25 21:26:36 +0000

    commit

--- function_app.py
+++ function_app.py
@@ -12,3 +12,25 @@ def execute_time(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed')
+
+
+@app.route(route="HelloWorld", auth_level=func.AuthLevel.ANONYMOUS)
+def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
