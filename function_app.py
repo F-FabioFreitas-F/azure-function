527579e3b673fbdac59ce5d29e996fbe26d2d57a
F-FabioFreitas-F <120140384+F-FabioFreitas-F@users.noreply.github.com>
2023-09-26 17:00:48 +0000

    test commit

--- function_app.py
+++ function_app.py
@@ -34,3 +34,13 @@ def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+
+@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def teste(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed')
